5ef95ac322c245da8f5ac73e0fa351e2fdff309d
Sunny Golovine <sunny@glvn.co>
2021-06-10 17:27:30 -0400

    WIP dark theme

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,11 +10,11 @@ interface Props {
 
 const Card: React.FC<Props> = ({ title, url, description, tags }) => (
   <div className="border shadow rounded-lg h-72 sm:w-72 p-4 m-2 overflow-hidden">
-    <h1 className="text-lg font-bold">{title}</h1>
-    <a href={url} className="text-sm text-blue-500 hover:underline w-72">
+    <h1 className="text-lg font-bold text-theme-white">{title}</h1>
+    <a href={url} className="text-sm text-theme-yellow hover:underline w-72">
       {url}
     </a>
-    <p className="py-2">{description}</p>
+    <p className="py-2 text-theme-white">{description}</p>
     <div className="flex flex-row flex-wrap py-4">
       {tags.map(item => {
         return <Tag key={item}>{item}</Tag>
--- src/components/card/Tag.tsx
+++ src/components/card/Tag.tsx
@@ -9,13 +9,13 @@ const Tag: React.FC<Props> = ({ children }) => {
   const { activeTag, setActiveTag } = useContext(SearchContext)
   const active = children === activeTag?.name
   // TODO: Move this to use classnames
-  const bg = active ? "bg-green-500" : "bg-blue-500"
+  const bg = active ? "bg-theme-yellow" : "bg-theme-white"
   return (
     <button
       onClick={() => setActiveTag({ name: children, occurrence: 1 })}
       className={`shadow rounded-lg py-1 px-3 ${bg} m-1`}
     >
-      <p className="text-xs text-white font-bold">{children}</p>
+      <p className="text-xs text-theme-dark font-bold">{children}</p>
     </button>
   )
 }
